a6da9ae085df29ac19e3f00b55cd78958a4a6dcb
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-12-01 21:16:00 +0200

    feat(BrainTest): prevent clearing the game header
    body: clear the screen without clearing the game header and show the number of score and trial

--- 1-brain-test-game/V2/main.cpp
+++ 1-brain-test-game/V2/main.cpp
@@ -47,6 +47,20 @@ int calc_timeout(int level)
     }
 }
 
+// Create game header
+void create_game_header()
+{
+    cout << "\t\t***Welcome to my first Game :)****\n";
+    cout << "\tVision the Shape and Calculate its Area before the time is over! \n";
+}
+
+// show the number of the current trial and the score value
+void show_score_trial(int n_trial, int m_trial, int score = 0)
+{
+    cout << "\n\t\tTrial: " << n_trial << "/" << m_trial;
+    cout << "\t\t Score: " << (score * 1.0 / m_trial) * 100.0 << "%" << endl;
+}
+
 // Main
 int main()
 {
@@ -62,7 +76,7 @@ int main()
     // 1a. Initiate Variables
     table_size = 5;
     m_trial = 5;
-    n_trial = 0;
+    n_trial = 1;
     score = 0;
 
     // 0b. Change random based on time
@@ -70,17 +84,16 @@ int main()
 
     // 0c. Print Game instructions
     system("cls");
-    cout << "\t\t***Welcome to my first Game :)****\n";
-    cout << "\tVision the Shape and Calculate its Area before the time is over! \n";
+    create_game_header();
     cout << "\n\nPress Enter to start playing ...";
     cin.get();
 
     // 0d. Show levels menu
-    cout << "Choose a level" << endl;
+    cout << "\nChoose the game level" << endl;
     cout << "Easy => 1" << endl;
     cout << "Medium => 2" << endl;
     cout << "Hard => 3" << endl;
-    cout << "Enter level number(1-3): ";
+    cout << "Enter the level number(1-3): ";
     cin >> level; // read game level value
 
     // validate the input value
@@ -90,6 +103,7 @@ int main()
         cout << "Please enter valid value" << endl;
         abort();
     }
+
     system("cls");
 
     // calculate game timeout according to the game level
@@ -97,14 +111,17 @@ int main()
 
     do
     {
+        // Create Header
+        create_game_header();
+        show_score_trial(n_trial, m_trial, score);
+
         //------Initiate Variables repeated part
         width = calc_table_side(level, table_size);
         length = calc_table_side(level, table_size);
         area = width * length;
-        n_trial++;
 
         // 2b. padding top
-        cout << "\n\n\n";
+        cout << "\n\n";
 
         // 2a. Draw the Rectangle
         for (int i = 0; i < length; i++)
@@ -120,9 +137,11 @@ int main()
 
         // 3c. Set Start time
         user_stime = time(0);
+
         // 3a. Ask User
         cout << "Area = ";
         cin >> user_area;
+
         // 3c. Set end time and calc diff
         user_timeout = time(0) - user_stime;
         cout << "You take " << user_timeout << "s to answer!....\n";
@@ -146,22 +165,26 @@ int main()
         sleep(3);
         system("cls");
 
-        //if the trial is not the last trial
-        if (n_trial < m_trial-1)
+        // if the trial is not the last trial
+        if (n_trial < m_trial)
         {
             for (int i = 3; i > 0; i--)
             {
+                create_game_header();
+                show_score_trial(n_trial, m_trial, score);
                 cout << "\n\t\tStart Next in " << i << "s...\n";
                 sleep(1);
                 system("cls");
             }
         }
-
+        n_trial++;
     }
     // 5a. Check the number of trial
-    while (n_trial < m_trial);
+    while (n_trial <= m_trial);
 
     // 6a. Game over and print score
+    // Create Header
+    create_game_header();
     cout << "\t\tGAME OVER\n";
     cout << "\t\tYour Score is :" << (score * 1.0 / m_trial) * 100.0 << "%\n";
 
